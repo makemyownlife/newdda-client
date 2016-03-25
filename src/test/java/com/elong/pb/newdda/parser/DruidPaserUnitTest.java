559273c61f29ff4eb7e71db7417f927932900cb4
zhangyong <zhangyong7120180@163.com>
2016-03-25 21:54:22 +0800

    mybatis druid 的处理

--- src/test/java/com/elong/pb/newdda/parser/DruidPaserUnitTest.java
+++ src/test/java/com/elong/pb/newdda/parser/DruidPaserUnitTest.java
@@ -23,4 +23,5 @@ public class DruidPaserUnitTest {
         List stmtList = parser.parseStatementList();
     }
 
+
 }
---
+++ src/test/java/com/elong/pb/newdda/plugin/MybatisUnitTest.java
@@ -0,0 +1,14 @@
+package com.elong.pb.newdda.plugin;
+
+/**
+ * mybatis 单元测试
+ * User: zhangyong
+ * Date: 2016/3/25
+ * Time: 21:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class MybatisUnitTest {
+
+
+
+}
