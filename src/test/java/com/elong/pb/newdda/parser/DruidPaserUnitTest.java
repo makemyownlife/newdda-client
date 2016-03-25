37b740baff4331dcac03328c2141b7f662cc1c84
zhangyong <zhangyong7120180@163.com>
2016-03-25 20:48:22 +0800

    改动相关的记录

---
+++ src/test/java/com/elong/pb/newdda/parser/DruidPaserUnitTest.java
@@ -0,0 +1,26 @@
+package com.elong.pb.newdda.parser;
+
+import com.alibaba.druid.sql.parser.SQLParserUtils;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.util.JdbcUtils;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangyong
+ * Date: 2016/3/25
+ * Time: 20:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class DruidPaserUnitTest {
+
+    @Test
+    public void testSelectStatement() {
+        String sql = "select *  from user where id = 1";
+        SQLStatementParser parser = SQLParserUtils.createSQLStatementParser(sql, JdbcUtils.MYSQL);
+        List stmtList = parser.parseStatementList();
+    }
+
+}
