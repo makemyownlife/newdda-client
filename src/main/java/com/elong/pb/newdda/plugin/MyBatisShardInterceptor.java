297060d6c757d7d41c91df1fde3b7eccfc79a615
zhangyong <zhangyong7120180@163.com>
2016-03-27 20:21:40 +0800

    其实 就是现在的生活 我想要的

---
+++ src/main/java/com/elong/pb/newdda/plugin/MyBatisShardInterceptor.java
@@ -0,0 +1,42 @@
+package com.elong.pb.newdda.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Properties;
+
+/**
+ * mybatis分区拦截器
+ * User: zhangyong
+ * Date: 2016/3/26
+ * Time: 20:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyBatisShardInterceptor implements Interceptor {
+
+    private static Logger logger = LoggerFactory.getLogger(MyBatisShardInterceptor.class);
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        logger.error("target:{}", target);
+        if (target instanceof Executor) {
+            return target;
+            //return Plugin.wrap(target, this);
+        } else {
+            return target;
+        }
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+
+}
---
+++ src/test/java/com/elong/pb/newdda/parser/JdbcUnitTest.java
@@ -0,0 +1,49 @@
+package com.elong.pb.newdda.parser;
+
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+/**
+ * jdbc 单元测试
+ * User: zhangyong
+ * Date: 2016/3/27
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class JdbcUnitTest {
+
+    @Test
+    public void testJdbcConnect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://10.100.19.144:3306/wiki";
+        String username = "root";
+        String password = "ilxw";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = null;
+            rs = null;
+            statement = connection.prepareStatement("select * from test where id = ?");
+            statement.setInt(1, 1);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("id"));
+            }
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+    }
+}
+
