3ee0251f0900f0cbb9282b466e21c7e5ee6456b5
zhangyong04 <zhangyong7120180@163.com>
2016-08-02 22:18:21 +0800

    SqlParserFactory 解析类 。

--- src/main/java/com/elong/pb/newdda/client/router/parser/SqlParserFactory.java
+++ src/main/java/com/elong/pb/newdda/client/router/parser/SqlParserFactory.java
@@ -1,6 +1,11 @@
 package com.elong.pb.newdda.client.router.parser;
 
 import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.db2.parser.DB2StatementParser;
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.dialect.oracle.parser.OracleStatementParser;
+import com.alibaba.druid.sql.dialect.sqlserver.parser.SQLServerStatementParser;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
 import com.elong.pb.newdda.client.constants.DatabaseType;
 import com.elong.pb.newdda.client.exception.SQLParserException;
 import org.slf4j.Logger;
@@ -20,11 +25,28 @@ public final class SqlParserFactory {
         if (logger.isDebugEnabled()) {
             logger.debug("Logic SQL: {}", sql);
         }
-        SQLStatement sqlStatement = null;
+        SQLStatementParser sqlStatementParser = getSQLStatementParser(databaseType, sql);
+        SQLStatement sqlStatement = sqlStatementParser.parseStatement();
         if (logger.isDebugEnabled()) {
             logger.trace("Get {} SQL Statement", sqlStatement.getClass().getName());
         }
         return null;
+    }
+
+    private static SQLStatementParser getSQLStatementParser(final DatabaseType databaseType, final String sql) {
+        switch (databaseType) {
+            case H2:
+            case MySQL:
+                return new MySqlStatementParser(sql);
+            case Oracle:
+                return new OracleStatementParser(sql);
+            case SQLServer:
+                return new SQLServerStatementParser(sql);
+            case DB2:
+                return new DB2StatementParser(sql);
+            default:
+                throw new UnsupportedOperationException(String.format("Cannot support database type [%s]", databaseType));
+        }
     }
 
 
