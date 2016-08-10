6f07f540f473d793a2ea84d32b906aec821f86f2
zhangyong <zhangyong7120180@163.com>
2016-08-10 21:12:58 +0800

    select 相关的内容

--- src/test/java/com/elong/pb/newdda/client/parser/SelectParserUnitTest.java
+++ src/test/java/com/elong/pb/newdda/client/parser/SelectParserUnitTest.java
@@ -1,10 +1,28 @@
 package com.elong.pb.newdda.client.parser;
 
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 /**
  * select statement的描述
  * Created by zhangyong on 2016/8/3.
  */
 public class SelectParserUnitTest {
 
+    @Test
+    public void testSelect() throws Exception {
+        String sql = "select * from person where name = 'xx' AND gender = 'M'";
+
+        SQLStatementParser parser = new MySqlStatementParser(sql);
+        List<SQLStatement> stmtList = parser.parseStatementList();
+        Assert.assertEquals(1, stmtList.size());
+
+    }
 
 }
