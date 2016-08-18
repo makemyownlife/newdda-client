0e34913fa63c1d2cecc61bc4e7cba15e34ca4162
zhangyong04 <zhangyong7120180@163.com>
2016-08-18 11:26:56 +0800

    添加解析的上下文 1

---
+++ src/main/java/com/elong/pb/newdda/client/router/parser/visitor/ParseContext.java
@@ -0,0 +1,14 @@
+package com.elong.pb.newdda.client.router.parser.visitor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 解析的上下文
+ * Created by zhangyong on 2016/8/18.
+ */
+public class ParseContext {
+
+    private final static Logger logger = LoggerFactory.getLogger(ParseContext.class);
+
+}
