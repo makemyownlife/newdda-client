10d2e41acc032234c9ce6e31d66678a014734848
zhangyong <zhangyong7120180@163.com>
2016-09-15 22:55:11 +0800

    条件上下文 去掉 google 相关的内容

--- src/main/java/com/elong/pb/newdda/client/router/result/router/ConditionContext.java
+++ src/main/java/com/elong/pb/newdda/client/router/result/router/ConditionContext.java
@@ -1,6 +1,5 @@
 package com.elong.pb.newdda.client.router.result.router;
 
-import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,8 +12,6 @@ import java.util.Map;
  * Created by zhangyong on 16/9/15.
  */
 public class ConditionContext {
-
-    private final static Logger logger = LoggerFactory.getLogger(ConditionContext.class);
 
     private final Map<RouterColumn, RouterCondition> conditions = new LinkedHashMap<RouterColumn, RouterCondition>();
 
