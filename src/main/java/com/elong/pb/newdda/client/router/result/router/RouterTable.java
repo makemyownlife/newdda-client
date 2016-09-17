c4224b61e6c86d965be25b6581ea7fb076eb3e12
zhangyong04 <zhangyong7120180@163.com>
2016-09-17 12:05:41 +0800

    路由 表  相关的 内容

--- src/main/java/com/elong/pb/newdda/client/router/result/router/RouterTable.java
+++ src/main/java/com/elong/pb/newdda/client/router/result/router/RouterTable.java
@@ -23,4 +23,38 @@ public class RouterTable {
         return alias;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.name == null ? 0 : this.name.hashCode());
+        result = prime * result + (this.alias == null ? 0 : this.alias.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == null || !(obj instanceof RouterTable)) {
+            return false;
+        }
+
+        RouterTable other = (RouterTable) obj;
+        if (this.name == null && other.getName() != null) {
+            return false;
+        }
+        if(!this.name.equals(other.getName())) {
+            return false;
+        }
+
+        if (this.alias == null && other.getAlias() != null) {
+            return false;
+        }
+        if(!this.alias.equals(other.getAlias())) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
