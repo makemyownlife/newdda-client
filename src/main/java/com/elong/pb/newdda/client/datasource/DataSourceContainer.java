fdd9421eb244195eb5b947e543ea0dd81aa903f9
zhangyong04 <zhangyong7120180@163.com>
2016-09-22 11:20:16 +0800

    分区1  分区2  分区3

--- src/main/java/com/elong/pb/newdda/client/datasource/DataSourceContainer.java
+++ src/main/java/com/elong/pb/newdda/client/datasource/DataSourceContainer.java
@@ -3,7 +3,9 @@ package com.elong.pb.newdda.client.datasource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,7 +16,19 @@ public class DataSourceContainer {
 
     private final static Logger logger = LoggerFactory.getLogger(DataSourceContainer.class);
 
+    //主从数据源列表
+    private List<MasterSlaveDataSource> masterSlaveDataSourceList = new ArrayList<MasterSlaveDataSource>();
+
     //所有的数据源
     private final Map<String, MasterSlaveDataSource> container = new HashMap<String, MasterSlaveDataSource>();
+
+    //===============================================================get set method start =========================================================================
+    public List<MasterSlaveDataSource> getMasterSlaveDataSourceList() {
+        return masterSlaveDataSourceList;
+    }
+    public void setMasterSlaveDataSourceList(List<MasterSlaveDataSource> masterSlaveDataSourceList) {
+        this.masterSlaveDataSourceList = masterSlaveDataSourceList;
+    }
+    //===============================================================get set method end =========================================================================
 
 }
