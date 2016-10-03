8ad219d878e3db9b47e2c090b051ef89ef561552
zhangyong <zhangyong7120180@163.com>
2016-10-03 22:06:47 +0800

    分区写完 ,明天 是result 相关的内容

--- src/main/java/com/elong/pb/newdda/client/router/rule/AlgorithmResult.java
+++ src/main/java/com/elong/pb/newdda/client/router/rule/AlgorithmResult.java
@@ -9,34 +9,12 @@ public class AlgorithmResult {
     //命中数据源
     private String targetDataSource;
 
-    //逻辑表名
-    private String logicTableName;
-
-    //替换后的表名
-    private String replaceTableName;
-
     public String getTargetDataSource() {
         return targetDataSource;
     }
 
     public void setTargetDataSource(String targetDataSource) {
         this.targetDataSource = targetDataSource;
-    }
-
-    public String getLogicTableName() {
-        return logicTableName;
-    }
-
-    public void setLogicTableName(String logicTableName) {
-        this.logicTableName = logicTableName;
-    }
-
-    public String getReplaceTableName() {
-        return replaceTableName;
-    }
-
-    public void setReplaceTableName(String replaceTableName) {
-        this.replaceTableName = replaceTableName;
     }
 
 }
--- src/main/java/com/elong/pb/newdda/client/router/rule/RangeAlgorithm.java
+++ src/main/java/com/elong/pb/newdda/client/router/rule/RangeAlgorithm.java
@@ -1,6 +1,8 @@
 package com.elong.pb.newdda.client.router.rule;
 
+import com.elong.pb.newdda.client.exception.ShardingJdbcException;
 import com.elong.pb.newdda.client.router.action.ShardingValue;
+import com.elong.pb.newdda.client.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +22,11 @@ public class RangeAlgorithm implements Algorithm {
     //分区列表
     private List<String> rangeList;
 
+    private static final int PARTITION_LENGTH = 1024;
+
+    // %转换为&操作的换算数值
+    private static final long AND_VALUE = PARTITION_LENGTH - 1;
+
     //================================================================set  set method  start ====================================================================
     public List<String> getDataSourceList() {
         return dataSourceList;
@@ -27,8 +34,40 @@ public class RangeAlgorithm implements Algorithm {
 
     @Override
     public AlgorithmResult doAlgorithm(ShardingValue shardingValue) {
+        if(dataSourceList.size() != rangeList.size()) {
+            throw new ShardingJdbcException("doAlgorithm error , 数据源数目与分区配置不一致!");
+        }
+        //将所有的参数解析成string 用string来hash
+        String valueStr = null;
         Object obj = shardingValue.getValue();
-        return null;
+        if(obj instanceof Number ) {
+            valueStr = String.valueOf(obj);
+        }
+        if(obj instanceof String) {
+            valueStr = (String)obj;
+        }
+
+        //计算hash值
+        long hash = StringUtil.hash(valueStr , 0 , valueStr.length());
+
+        //目标数据源
+        String targetDataSource = null;
+        //在虚拟的1024个分区中 查看位于哪个分区中
+        int temp  = (int) (hash & AND_VALUE);
+        for(int i = 0 ;i < rangeList.size() ; i++) {
+            String range = rangeList.get(i);
+            String[] arr = range.split(",");
+            int start = Integer.valueOf(arr[0]);
+            int end = Integer.valueOf(arr[1]);
+            if(temp >= start && temp <= end) {
+                targetDataSource = dataSourceList.get(i);
+                break;
+            }
+        }
+
+        AlgorithmResult algorithmResult = new AlgorithmResult();
+        algorithmResult.setTargetDataSource(targetDataSource);
+        return algorithmResult;
     }
 
     public void setDataSourceList(List<String> dataSourceList) {
@@ -42,6 +81,7 @@ public class RangeAlgorithm implements Algorithm {
     public void setRangeList(List<String> rangeList) {
         this.rangeList = rangeList;
     }
+
     //================================================================set  set method  end ====================================================================
 
 }
